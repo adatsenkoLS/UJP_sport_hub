bf733235929da7541b80a1f413ab97c9759e5d29
kostvick <my.mail.kostya@gmail.com>
2021-09-16 19:23:28 +0300

    sport_hub

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM ruby:3.0.0
 RUN apt-get update -qq && apt-get install -y nodejs postgresql-client npm
 RUN npm install -g yarn 
 WORKDIR /UJP_SPORT_HUB
-COPY Gemfile /UJP_SPORT_HUB/Gemfile
-COPY Gemfile.lock /UJP_SPORT_HUB/Gemfile.lock
+COPY . .
+COPY . .
 RUN gem install bundler
 RUN bundle install
 RUN rails assets:precompile
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,9 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -53,7 +52,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -88,7 +87,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
