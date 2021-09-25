ad81361900e10261596de0bb0a2256cdb4a261a9
pomello0o <myroslav.trynoha.pm.2019@lpnu.ua>
2021-09-25 13:07:52 +0300

    Created default navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  get 'cabinet/personal', to: 'cabinet#personal'
+  get 'cabinet/password', to: 'cabinet#password'
+  get 'cabinet/surveys', to: 'cabinet#surveys'
+  get 'cabinet/teamhub', to: 'cabinet#teamhub'
 end
