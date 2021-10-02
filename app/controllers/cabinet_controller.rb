6cd22296aa20a6fa6928d7ac51580cfc2a140af3
pomello0o <myroslav.trynoha.pm.2019@lpnu.ua>
2021-10-02 15:15:55 +0300

    Updating cabinet

--- app/controllers/cabinet_controller.rb
+++ app/controllers/cabinet_controller.rb
@@ -2,17 +2,11 @@ class CabinetController < ApplicationController
     #before_action :authenticate_user!
 
     def personal
-=begin
-        @avatar = current_user[:avatar]
-        @first_name = current_user[:first_name]
-        @last_name = current_user[:last_name]
-        @email = current_user[:email]
+=begin    
+        @first_name = current_user.first_name
+        @last_name = current_user.last_name
+        @email = current_user.email
 =end
-    end
-
-    def update_profile
-        comments_from_form = params['myform']['fname']
-        @first_name = comments_from_form
     end
 
     def password
