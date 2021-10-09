f7e6fcf7c8a633a62c8ccb68fff8ea00c307ec97
Andrii Datsenko <adatse@softserveinc.com>
2021-10-09 14:03:39 +0300

    fix naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  devise_for :users ,path:'auth',:controllers => { :sessions => 'users/sessions',
-      :passwords => 'users/passwords'},
+  devise_for :users ,path:'auth',:controllers => { :sessions => 'user/sessions',
+      :passwords => 'user/passwords'},
    path_names:
    {
     sign_in: 'login'
