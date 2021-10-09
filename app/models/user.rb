3e8b3b4e57daf757d6bbb1d99d6e375b8fecc115
kostvick <my.mail.kostya@gmail.com>
2021-10-09 13:36:42 +0300

    set min password length to 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   # :confirmable, :rememberable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable, :lockable, :timeoutable, :trackable, :omniauthable
+         :recoverable, :validatable, :lockable, :timeoutable, :trackable, :omniauthable, password_length: 8..128
 
   def name
     [first_name, last_name].join(' ')
