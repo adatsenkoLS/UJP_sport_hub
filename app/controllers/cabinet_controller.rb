a34e50a0b667e3c28a531f3c487e59b394c2269e
pomello0o <myroslav.trynoha.pm.2019@lpnu.ua>
2021-10-10 15:33:37 +0300

    Updating cabinet

--- app/controllers/cabinet_controller.rb
+++ app/controllers/cabinet_controller.rb
@@ -1,11 +1,10 @@
 class CabinetController < ApplicationController
-    #before_action :authenticate_user!
+    before_action :authenticate_user!
 
     def personal
     end
 
     def update
-        current_user = User.new
         current_user.first_name = params[:first_name]
         current_user.last_name = params[:last_name]
         current_user.email = params[:email]
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-   
-     scope '(:locale)', locale: /en||ua/ do
-       devise_for :users, skip: :omniauth_callbacks
-     end
-       devise_for :users, skip: :omniauth_callbacks
-     end
+
+  scope '(:locale)', locale: /en||ua/ do
+    devise_for :users, skip: :omniauth_callbacks
+  end
 
   root 'home_page#home'
 
