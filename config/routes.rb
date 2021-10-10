4299b159cd66288c3720248d9a79f9b77ed19cbb
bob <yura.kondak@gmail.com>
2021-10-10 15:16:59 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, only: :omniauth_callbacks, 
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+  :registrations => 'users/registrations' }
 
   scope '(:locale)', locale: /en||ua/ do
-    devise_for :users, skip: :omniauth_callbacks
+    devise_for :users, skip: :omniauth_callbacks,controllers: {:registrations => 'users/registrations' }
   end
 
   root 'home_page#home'
