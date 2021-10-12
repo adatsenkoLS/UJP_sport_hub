cd53d027f6c7261a6f47b18df69ec80624cf5241
pomello0o <myroslav.trynoha.pm.2019@lpnu.ua>
2021-10-12 13:43:32 +0300

    Updating cabinet

--- app/controllers/cabinet_controller.rb
+++ app/controllers/cabinet_controller.rb
@@ -1,13 +1,14 @@
 class CabinetController < ApplicationController
-    before_action :authenticate_user!
+    before_action :logged_in_user, only: [:edit, :update]
 
     def personal
     end
 
     def update
-        current_user.first_name = params[:first_name]
-        current_user.last_name = params[:last_name]
-        current_user.email = params[:email]
+        @user = User.find(params[:id])
+        @user.first_name = params[:first_name]
+        @user.last_name = params[:last_name]
+        @user.email = params[:email]
 
         redirect_back(fallback_location: root_path)
     end
@@ -29,4 +30,18 @@ class CabinetController < ApplicationController
 
     def teamhub
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+
+    private
+
+    def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
+      end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_010_110_131) do
+ActiveRecord::Schema.define(version: 2021_10_10_110131) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table "sessions", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -48,9 +49,11 @@ ActiveRecord::Schema.define(version: 20_211_010_110_131) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
+
 end
