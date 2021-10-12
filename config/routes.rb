1e22f2d2b8f6234dd978f63dcde59db9beec77a9
kostvick <my.mail.kostya@gmail.com>
2021-10-12 18:14:05 +0300

    changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-
-  devise_for :user,path:'auth', only: :omniauth_callbacks, 
-  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-  :registrations => 'user/registrations', :sessions => 'user/sessions' }
+  devise_for :user, path: 'auth', only: :omniauth_callbacks,
+                    controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                                   registrations: 'user/registrations', sessions: 'user/sessions' }
 
   scope '(:locale)', locale: /en||ua/ do
-    devise_for :user,path:'auth', skip: :omniauth_callbacks,controllers:
-    { :registrations => 'user/registrations', 
-      :sessions => 'user/sessions',
-      :passwords => 'user/passwords' }
+    devise_for :user, path: 'auth', skip: :omniauth_callbacks, controllers:
+    { registrations: 'user/registrations',
+      sessions: 'user/sessions',
+      passwords: 'user/passwords' }
   end
 
   root 'home_page#home'
