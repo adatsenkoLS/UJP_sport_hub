165555b8a195d49049f2f9b4fb4be7868ea987b4
kostvick <my.mail.kostya@gmail.com>
2021-10-12 18:13:40 +0300

    role for user and seed for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
+  # Roles
+  enum role: %i[user admin]
+  after_initialize :set_default_role, if: :new_record?
   # Include default devise modules. Others available are:
-
   # :confirmable, :rememberable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :validatable, :lockable, :timeoutable, :trackable, :omniauthable, password_length: 8..128
+
+  def set_default_role
+    self.role ||= :user
+  end
 
   def name
     [first_name, last_name].join(' ')
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_110131) do
+ActiveRecord::Schema.define(version: 2021_10_12_135830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_110131) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "role"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
